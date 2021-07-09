554df80d958f3f2751248b02249792ef53be97a3
hiro21 <hiswing8@gmail.com>
2021-07-09 21:06:31 +0900

    add di

--- src/Application.kt
+++ src/Application.kt
@@ -2,12 +2,24 @@ package com.example
 
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import org.koin.ktor.ext.Koin
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    // setting DI
+    install(Koin) {
+
+    }
+    routing {
+        get("/test") {
+            call.apply {
+                respond("helloTest")
+            }
+        }
+    }
 }
 
